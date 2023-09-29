7df78d4cf433e3a317e8117a8fb83a55a29e0d29
SupremeWarMan <146172547+SupremeWarMan@users.noreply.github.com>
2023-09-30 00:57:26 +0100

    made better

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+// Function to convert binary to decimal
+function binaryToDecimal(binary) {
+    return parseInt(binary, 2);
+}
+
+// Function to convert decimal to binary
+function decimalToBinary(decimal) {
+    return (decimal >>> 0).toString(2);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+    const binaryInput = document.getElementById('binaryInput');
     const decimalInput = document.getElementById('decimalInput');
-    const binaryOutput = document.getElementById('binaryOutput');
-    const convertButton = document.getElementById('convertButton');
+    const convertToDecimalBtn = document.getElementById('convertToDecimal');
+    const convertToBinaryBtn = document.getElementById('convertToBinary');
 
-    convertButton.addEventListener('click', function () {
-        const decimalValue = parseInt(decimalInput.value);
-
-        if (!isNaN(decimalValue)) {
-            const binaryValue = decimalValue.toString(2);
-            binaryOutput.value = binaryValue;
+    // Convert binary to decimal
+    convertToDecimalBtn.addEventListener('click', function () {
+        const binaryValue = binaryInput.value;
+        if (/^[01]+$/.test(binaryValue)) {
+            decimalInput.value = binaryToDecimal(binaryValue);
         } else {
-            binaryOutput.value = 'Invalid Input';
+            alert('Please enter a valid binary number (0s and 1s only).');
+        }
+    });
+
+    // Convert decimal to binary
+    convertToBinaryBtn.addEventListener('click', function () {
+        const decimalValue = parseInt(decimalInput.value);
+        if (!isNaN(decimalValue)) {
+            binaryInput.value = decimalToBinary(decimalValue);
+        } else {
+            alert('Please enter a valid decimal number.');
         }
     });
 });
