f4c9b98f57f93bb10b4b2cd4471c3c78e334af00
SupremeWarMan <146172547+SupremeWarMan@users.noreply.github.com>
2023-09-30 11:35:12 +0100

    commit index.html

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-// Function to convert binary to decimal
-function binaryToDecimal(binary) {
-    return parseInt(binary, 2);
-}
-
-// Function to convert decimal to binary
-function decimalToBinary(decimal) {
-    return (decimal >>> 0).toString(2);
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    const binaryInput = document.getElementById('binaryInput');
-    const decimalInput = document.getElementById('decimalInput');
-    const convertToDecimalBtn = document.getElementById('convertToDecimal');
-    const convertToBinaryBtn = document.getElementById('convertToBinary');
+    const conversionInput = document.getElementById('conversionInput');
+    const conversionOutput = document.getElementById('conversionOutput');
+    const convertBtn = document.getElementById('convert');
+    const binaryToDecimalRadio = document.getElementById('binaryToDecimal');
+    const decimalToBinaryRadio = document.getElementById('decimalToBinary');
 
-    // Convert binary to decimal
-    convertToDecimalBtn.addEventListener('click', function () {
-        const binaryValue = binaryInput.value;
-        if (/^[01]+$/.test(binaryValue)) {
-            decimalInput.value = binaryToDecimal(binaryValue);
-        } else {
-            alert('Please enter a valid binary number (0s and 1s only).');
-        }
-    });
+    // Function to convert binary to decimal
+    function binaryToDecimal(binary) {
+        return parseInt(binary, 2);
+    }
 
-    // Convert decimal to binary
-    convertToBinaryBtn.addEventListener('click', function () {
-        const decimalValue = parseInt(decimalInput.value);
-        if (!isNaN(decimalValue)) {
-            binaryInput.value = decimalToBinary(decimalValue);
+    // Function to convert decimal to binary
+    function decimalToBinary(decimal) {
+        return (decimal >>> 0).toString(2);
+    }
+
+    // Convert on button click
+    convertBtn.addEventListener('click', function () {
+        const input = conversionInput.value.trim();
+        if (binaryToDecimalRadio.checked && /^[01]+$/.test(input)) {
+            // Convert binary to decimal
+            conversionOutput.value = binaryToDecimal(input);
+        } else if (decimalToBinaryRadio.checked && /^\d+$/.test(input)) {
+            // Convert decimal to binary
+            conversionOutput.value = decimalToBinary(parseInt(input));
         } else {
-            alert('Please enter a valid decimal number.');
+            alert('Please enter a valid number and select the conversion type.');
         }
     });
 });
+
